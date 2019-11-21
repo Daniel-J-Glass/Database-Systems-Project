254f9562e2a0c1d9d82b69b7ad173c3a3e722698
Daniel-J-Glass <DanieljGlass1@gmail.com>
2019-11-21 10:42:09 -0600

    Put my name on stuff

--- LibraryDump(Fixed).sql
+++ LibraryDump(Fixed).sql
@@ -1,3 +1,4 @@
+--Daniel Glass
 CREATE DATABASE  IF NOT EXISTS `library` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `library`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
